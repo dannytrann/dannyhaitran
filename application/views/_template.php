b5c42be5f2e6cdce95a4ee9fea29d5639530d62a
Danny Tran <dannyhaitran@outlook.com>
2015-01-16 21:58:52 -0800

    added google analytics

--- application/views/_template.php
+++ application/views/_template.php
@@ -34,7 +34,9 @@ if (!defined('APPPATH'))
         <div id="about" class="parallax-window" data-parallax="scroll" data-image-src="/data/bg/sky.jpg">
             {my-info}
         </div>  
-        {footer}
+        <div>
+            {footer}
+        </div>
         <script src="/assets/js/jquery-1.11.1.min.js"></script>
         <script src="/assets/js/bootstrap.min.js"></script>
         <script src="/assets/js/parallax.min.js"></script>
@@ -55,5 +57,15 @@ if (!defined('APPPATH'))
                 }
             });
         </script>
+        <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-58656341-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
     </body>
 </html>
