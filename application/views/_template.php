c65f3a650ed027e8783eae14bc8f4412869413a2
Danny Tran <dannyhaitran@outlook.com>
2015-02-01 02:56:54 -0800

    removed the analytics

--- application/views/_template.php
+++ application/views/_template.php
@@ -57,7 +57,7 @@ if (!defined('APPPATH'))
                 }
             });
         </script>
-        <script>
+      <!--  <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -66,6 +66,6 @@ if (!defined('APPPATH'))
   ga('create', 'UA-58656341-1', 'auto');
   ga('send', 'pageview');
 
-</script>
+</script> -->
     </body>
 </html>
